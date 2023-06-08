fdad590fb3dd1f13d1c0f0bc2dcd8ac73243497d
Italo Morales F <i@platzi.com>
2023-06-08 02:10:21 -0500

    paginación

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -28,6 +28,7 @@ use Symfony\Component\Validator\Constraints as Assert;
     ],
     // normalizationContext: ['groups' => ['read']], // GET
     denormalizationContext: ['groups' => ['write']], // POST, PUT, PATCH
+    paginationItemsPerPage: 8
 )]
 class Post
 {
