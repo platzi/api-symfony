9baf8c57802856de5189b2fd8203352ba533e379
Italo Morales F <i@platzi.com>
2023-06-08 01:45:18 -0500

    validación

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -12,6 +12,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 #[ApiResource(
@@ -38,15 +39,18 @@ class Post
 
     #[ORM\Column(length: 255)]
     #[Groups(['read', 'write'])]
+    #[Assert\NotBlank]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
     #[Groups(['read:item', 'write'])]
+    #[Assert\NotBlank]
     private ?string $body = null;
 
     #[ORM\ManyToOne(inversedBy: 'posts')]
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['read', 'write'])]
+    #[Assert\NotBlank]
     private ?Category $category = null;
 
     public function getId(): ?int
